a15050e91de48e9ce48839e9a084289fb217c906
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-07-31 22:11:37 +0700

    Thêm modal khi click vào buytickets

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const btn = document.querySelector('.menu_icon');
 const headerClick = document.getElementById('header');
 const menuItems = document.querySelectorAll('.list .list_items a[href*="#"]');
 
+// get btn ticket
+const btnBuy = document.querySelectorAll('.js-buy-ticket');
+const modalOut = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal-close');
+const modalContainer = document.querySelector('.modal_container');
+
 const heightHeader = headerClick.clientHeight;
 btn.addEventListener('click', () => {
     const isCloseHeader = headerClick.clientHeight === heightHeader;
@@ -10,12 +16,6 @@ btn.addEventListener('click', () => {
     }
     headerClick.style.height = null;
 });
-
-// menuItems.forEach((item) => {
-//     item.onclick = function () {
-//         console.log(item);
-//     };
-// });
 
 for (let i = 0; i < menuItems.length; i++) {
     const menuItem = menuItems[i];
@@ -31,3 +31,24 @@ for (let i = 0; i < menuItems.length; i++) {
         }
     };
 }
+
+// buy Tickets
+
+btnBuy.forEach((btn) => {
+    btn.onclick = function (e) {
+        e.preventDefault();
+        modalOut.classList.add('open');
+    };
+});
+
+closeModal.onclick = function () {
+    modalOut.classList.remove('open');
+};
+
+modalOut.onclick = function () {
+    modalOut.classList.remove('open');
+};
+
+modalContainer.onclick = function (e) {
+    e.stopPropagation();
+};
